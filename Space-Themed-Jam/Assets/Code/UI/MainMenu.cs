21d7869d1afb4ff7ad9f5a022840914d254ad50c
jhcanti <jhcanti@gmail.com>
2021-12-11 22:46:38 +0100

    Actualizo Main Menu básico

---
+++ Space-Themed-Jam/Assets/Code/UI/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private Button highScoresButton;
+    [SerializeField] private Button quitButton;
+
+    private EventQueue _eventQueue;
+        
+    private void Awake()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+        highScoresButton.onClick.AddListener(GoToHighScores);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    private void Start()
+    {
+        _eventQueue = ServiceLocator.Instance.GetService<EventQueue>();
+    }
+
+    private void GoToHighScores()
+    {
+    }
+
+    private void StartGame()
+    {
+        _eventQueue.EnqueueEvent(new StartGameEvent());
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+}
