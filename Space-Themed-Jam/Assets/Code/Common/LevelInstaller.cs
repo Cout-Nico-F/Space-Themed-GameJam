0e1574f94e1384a5200abfa0bc743b0dc25d3100
jhcanti <jhcanti@gmail.com>
2021-12-12 16:36:33 +0100

    Installer para el Gameplay

---
+++ Space-Themed-Jam/Assets/Code/Common/LevelInstaller.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LevelInstaller : MonoBehaviour
+{
+    [SerializeField] private EnemySpawner enemySpawner;
+    [SerializeField] private UISystem uiSystem;
+
+
+    private void Awake()
+    {
+        ServiceLocator.Instance.RegisterService(enemySpawner);
+        ServiceLocator.Instance.RegisterService(uiSystem);
+    }
+
+    private void OnDestroy()
+    {
+        ServiceLocator.Instance.UnregisterService<EnemySpawner>();
+        ServiceLocator.Instance.UnregisterService<UISystem>();
+    }
+}
