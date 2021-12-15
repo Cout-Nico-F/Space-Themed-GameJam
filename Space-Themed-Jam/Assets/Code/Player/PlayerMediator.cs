daeed35a181732568a0ae8e96cd0525476393932
jhcanti <jhcanti@gmail.com>
2021-12-15 19:41:23 +0100

    Update PlayerMediator.cs

--- Space-Themed-Jam/Assets/Code/Player/PlayerMediator.cs
+++ Space-Themed-Jam/Assets/Code/Player/PlayerMediator.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class PlayerMediator : MonoBehaviour
 {
+    [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private WeaponController weaponController;
+
+    private IInput _input;
+    private Vector2 _direction;
+
+
+    private void Start()
+    {
+        _input = ServiceLocator.Instance.GetService<IInput>();
+    }
+
+
+    private void Update()
+    {
+        _direction = _input.GetDirection();
+
+        if (_input.IsActionFirePressed())
+        {
+            weaponController.TryShoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerMovement.Move(_direction);
+    }
 }
